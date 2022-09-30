33240e67a8318b3c2c0471b331c31ca7bcff907f
Valeriy <valeriy.gnybidenko@gmail.com>
2022-09-30 10:20:32 +0300

    feature: add portfolio-header: 30min

--- src/js/loaders/portfolioLoader.js
+++ src/js/loaders/portfolioLoader.js
@@ -3,12 +3,22 @@ import {TimelineMax, Expo} from 'gsap'
 
 const portfolioLoader = () => {
   const h1 = document.querySelector('h1')
+  const h2 = document.querySelector('.portfolio__h2')
   const content = document.querySelector('.portfolio')
   splitting({target: h1, by: 'chars'})
+  splitting({target: h2, by: 'chars'})
   const tl = new TimelineMax()
   tl.to(h1, 0.01, {opacity: 1}, 0.2)
+    .to(h2, 0.01, {opacity: 1}, 0.2)
     .staggerTo(
       h1.querySelectorAll('.char'),
+      1.8,
+      {x: 0, opacity: 1, ease: Expo.easeOut},
+      0.07,
+      0.2,
+    )
+    .staggerTo(
+      h2.querySelectorAll('.char'),
       1.8,
       {x: 0, opacity: 1, ease: Expo.easeOut},
       0.07,
